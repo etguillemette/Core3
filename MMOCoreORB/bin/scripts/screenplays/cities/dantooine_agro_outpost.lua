b5c728af0006c25293215aa5b524b82caff620aa
etguillemette <97071424+etguillemette@users.noreply.github.com>
2024-12-06 11:19:57 -0500

    Update dantooine_agro_outpost.lua
    Fixing Agro outpost junk dealer

--- MMOCoreORB/bin/scripts/screenplays/cities/dantooine_agro_outpost.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/dantooine_agro_outpost.lua
@@ -95,7 +95,7 @@ function DantooineAgroOutpostScreenPlay:spawnMobiles()
 		local pMobile = spawnMobile(self.planet, mob[1], mob[2], mob[3], mob[4], mob[5], mob[6], mob[7])
 
 		--Ethan edit 12-3-24 (NPC VENDOR) Adding vendors to adventure planets
-		local pNpc = spawnMobile("dantooine", "junk_dealer", 60, 1547, 4, -6441, 0, 3605972)
+		local pNpc = spawnMobile(self.planet, "junk_dealer", 60, 1547, 4, -6441, 0, 3605972)
 		if pNpc ~= nil then
 			AiAgent(pNpc):setConvoTemplate("junkDealerGenericConvoTemplate")
 		end
