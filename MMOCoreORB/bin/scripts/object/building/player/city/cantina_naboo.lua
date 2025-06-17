53ec780efbd47cf86614077e9dfa1f7099342858
etguillemette <97071424+etguillemette@users.noreply.github.com>
2025-06-17 08:57:12 -0400

    Update cantina_naboo.lua
    [IMAGE DESIGNER] Trying to make it so you can do image designer stuff here, like a salon

--- MMOCoreORB/bin/scripts/object/building/player/city/cantina_naboo.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/cantina_naboo.lua
@@ -48,6 +48,7 @@ object_building_player_city_cantina_naboo = object_building_player_city_shared_c
 	length = 7,
 	width = 7,
 	planetMapCategory = "cantina",
+	gameObjectType = 530, --Ethan edit (IMAGE DESIGNER) Trying to make it so you can do image designer stuff here, like a salon
 	cityRankRequired = 2,
 	limitToOnePerCity = 0,
 	abilityRequired = "place_cantina",
